1f2fd6a3eb5bf38c0dd9e196c557688bba3f4be5
nel <alopena55555@gmail.com>
2022-10-30 16:43:38 +0800

    jjdhfj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import {RiLoginCircleLine} from "react-icons/ri";
 
 function Header() {
     const [offset, setOffset] = useState(0);
-    const [innerHeight, setInnerHeight] = useState(0);
+    const [innerHeight, setInnerHeight] = useState(1);
 
     useEffect(() => {
         const onScroll = () => {
@@ -21,28 +21,28 @@ function Header() {
 
 
     return(
-        <div className={`flex h-16 sm:h-20 w-screen shadow-sm justify-center fixed transition duration-500 ease-in ${offset >= innerHeight ? "bg-white" : "bg-transparent"} z-[1000]`}>
+        <div className={`flex h-16 sm:h-20 w-screen justify-center transition duration-500 ease-in ${offset >= (innerHeight - 40) ? "bg-white shadow-sm hidden" : "bg-transparent fixed"} z-[1000]`}>
             <div className="flex max-w-[1200px] w-[100%]">
                 {/* Logo */}
                 <div className="flex p-3 sm:p-4 min-w-[25%] grow">
-                    <img src={Logo} alt="logo" className="w-10 h-10 sm:w-12 sm:h-12 ml-1 md:ml-4" />
-                    <div className={`text-2xl sm:text-3xl mt-1 ml-1 font-bold transition duration-500 ease-in ${offset >= innerHeight ? "text-slate-800" : "text-slate-200"}`}>Nel</div>
+                    <img src={Logo} alt="logo" className="w-10 h-10 sm:w-12 sm:h-12 ml-1 md:ml-4 transition duration-300 hover:scale-125" />
+                    <div className="text-2xl sm:text-3xl mt-1 ml-1 font-bold text-slate-200">Nel</div>
                 </div>
                 {/* Search */}
                 <div className="grow p-4 justify-start hidden md:flex">
                     <div className="border-slate-200 border-2 w-[90%] flex rounded-full overflow-hidden">
-                        <FiSearch size={30} className={`ml-4 mt-2 mr-2 text-slate-600 transition duration-500 ease-in ${offset >= innerHeight ? "text-slate-600" : "text-slate-300"}`}/>
-                        <input type="text" className="pl-4 pr-4 w-[100%] outline-none bg-transparent" placeholder="Search something..." />
+                        <FiSearch size={30} className="ml-4 mt-2 mr-2 text-slate-300"/>
+                        <input type="text" className="pl-4 pr-4 w-[100%] outline-none bg-transparent text-white" placeholder="Search something..." />
                         <span className="h-[65%] w-[1px] bg-slate-200 mt-2"></span>
-                        <button className={`p-2 font-bold pl-4 text-md pr-4 transition bg-slate-100 text-slate-600 hover:bg-slate-200 duration-500 ease-in ${offset >= innerHeight ? "opacity-100" : "opacity-50"}`}>Search</button>
+                        <button className="p-2 font-bold pl-4 text-md pr-4 bg-slate-100 text-slate-600 hover:bg-slate-200 opacity-50">Search</button>
                     </div>
                 </div>
                 {/* Btn or profile */}
                 <div className="flex p-3 sm:p-4">
-                    <FiSearch className={`ml-4 mt-2 mr-2 text-2xl sm:text-3xl text-slate-600 md:hidden transition duration-500 ease-in ${offset >= innerHeight ? "text-slate-600" : "text-slate-300"}`}/>
-                    <CgShoppingCart className={`mt-2 mr-2 text-2xl sm:text-3xl text-slate-600 transition duration-500 ease-in ${offset >= innerHeight ? "text-slate-600" : "text-slate-300"}`}/>
+                    <FiSearch className="ml-4 mt-2 mr-2 text-2xl sm:text-3xl md:hidden transition text-slate-300"/>
+                    <CgShoppingCart className="mt-2 mr-2 text-2xl sm:text-3xl transition text-slate-300"/>
                     <button className="p-1 mr-4 md:mr-6 font-bold pl-4 text-md pr-4 rounded-full w-32 transition bg-blue-600 text-slate-100 hover:bg-blue-700 hidden sm:block">Sign Up</button>
-                    <RiLoginCircleLine className="bg-blue-600 p-2 text-4xl text-slate-100 transition rounded-full hover:bg-blue-700 mt-[3px] mr-4 sm:hidden"/>
+                    <RiLoginCircleLine className="bg-blue-600 p-2 text-4xl text-slate-100 rounded-full hover:bg-blue-700 mt-[3px] mr-4 sm:hidden"/>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import OnlineShop from "../assets/online-shopping.svg";
 import Header from "../components/Header";
+import Products from "../components/Products";
 
 function Home(){
+    const scrTo = useRef(null);
+
     return(
         <>
         <Header/>
         <div className="h-screen w-screen rad-bg flex justify-center">
             <div className="max-w-[1200px] w-full flex">
                  {/* Text */}
-                <div className="h-full w-1/2 flex justify-center flex-col pl-8">
-                    <h1 className="text-slate-100 text-3xl font-semibold mb-1">Nel003 Shop</h1>
+                <div className="h-full sm:w-1/2 w-[100%] flex justify-center flex-col pl-8">
+                    <h1 className="text-slate-100 text-4xl font-semibold mb-1">Online Shoping</h1>
                     <span className="w-28 h-[1px] bg-slate-300 ml-1 mb-1"></span>
-                    <p className="text-slate-300 w-[7   0%] min-w-[200px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-                    <button className="text-slate-100 p-2 w-32 border-2 rounded-full transition border-blue-500 hover:bg-blue-500 mt-7">Shop Now</button>
+                    <p className="text-slate-300 text-lg w-[70%] min-w-[240px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                    <button onClick={() => scrTo.current?.scrollIntoView({behavior: 'smooth'})} className="text-slate-100 p-2 w-32 border-2 rounded-full transition border-blue-500 hover:bg-blue-500 mt-7">Shop Now</button>
                 </div>
                 {/* Svg */}
-                <div className="h-full w-1/2 flex justify-center align-center">
-                    <img src={OnlineShop}/>
+                <div className="h-full sm:w-1/2 w-0 flex justify-center align-center">
+                    <img className="hidden sm:block" src={OnlineShop}/>
                 </div>
             </div>
         </div>
-        <div className="h-screen w-screen flex bg-black"></div>
+        <div ref={scrTo} className="h-auto w-screen flex justify-center">
+            <div className="max-w-[1200px] w-full flex items-center mt-12 pl-6 pr-6 mb-12">
+                <Products/>
+            </div>
+        </div>
+        <div className="w-full h-screen"></div>
         </>
     )
 }
